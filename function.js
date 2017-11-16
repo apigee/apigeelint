50e6167b911a2ee3d006116c844b108ae88dc819
davidwallen@google.com <davidwallen@davidwallen-macbookpro.roam.corp.google.com>
2017-11-16 11:33:39 -0800

    Various fixes.

--- function.js
+++ function.js
@@ -6,27 +6,72 @@
  * 
  * gcloud beta functions deploy lints --trigger-http --stage-bucket lintresults --memory 2048
  */
+
 exports.lints = function lintResults(req, res) {
   const util = require("util");
 
-  var org,
+  var customer,
+    org,
     api,
     revision,
     authorization = req.get("Authorization"),
     formatter;
+
   if (req.method === "GET") {
+    customer = req.query.customer;
     org = req.query.org;
     api = req.query.api;
     revision = req.query.revision;
     formatter = req.query.formatter;
   } else if (req.method === "POST") {
+    customer = req.body.customer;
     org = req.body.organization;
     api = req.body.api;
     revision = req.body.revision;
     formatter = req.body.formatter;
   }
 
-  //look at interpretting the accepts header if the formatter was not explicitly supplied
+  //interpret the accepts header if the formatter was not explicitly supplied
+  if (!formatter) {
+    if (req.accepts("json")) {
+      formatter = "json.js";
+    } else if (req.accepts("xml")) {
+      formatter = "jslint-xml.js";
+    } else if (req.accepts("text")) {
+      formatter = "unix.js";
+    } else {
+      formatter = "html.js";
+    }
+  }
+
+  if (!org && customer) {
+    //populate from the customer if supplied
+    
+    var str = '';
+    
+      var options = {
+        host: 'nucleus-api-test.apigee.com',
+        path: '/sfdc/account_orgs'
+      };
+    
+      var callback = function(response) {
+        response.on('data', function (chunk) {
+          str += chunk;
+        });
+    
+        response.on('end', function () {
+          console.log(str);
+          //str contains the payload from the account_orgs - parse to get the orgs to lint
+          res.send(str); // SEND ACTUAL RESPONSE HERE
+        });
+      }
+    
+      var req = http.request(options, callback);
+      req.end();
+    
+  }else{
+
+  }
 
   if (!org || !api || !revision || !req.get("Authorization")) {
     // This is an error case, as "message" is required.
@@ -53,21 +98,25 @@ exports.lints = function lintResults(req, res) {
         authorization
       },
       output: function(report) {
-        res.status(200).send(report);
+        if (req.query.debug) {
+          console.log(report);
+        }
       },
       apiUpload: {
         destPath: "https://csdata-test.apigee.net/v1/lintresults",
         authorization,
         organization: org
       },
-      formatter: formatter || "html.js"
+      formatter
     };
 
     var bl = require("./lib/package/bundleLinter.js");
     bl.lint(configuration, function(result, err) {
-      if(err){
+      if (err) {
         console.log(util.inspect(err, { showHidden: false, depth: 3 }));
         res.status(err.status).send(err.message);
+      } else {
+        res.status(200).send(result.apiUploadResponse);
       }
     });
   }
