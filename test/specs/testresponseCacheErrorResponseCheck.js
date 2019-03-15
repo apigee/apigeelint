5b42062243e32638b19a3b7b9d7b817e2e865d0a
Artem Petrosian <artem@petrosian.net>
2019-03-15 09:19:09 -0700

    Add license header

--- test/specs/testresponseCacheErrorResponseCheck.js
+++ test/specs/testresponseCacheErrorResponseCheck.js
@@ -1,3 +1,19 @@
+/*
+  Copyright 2019 Google LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 var assert = require("assert"),
   decache = require("decache"),
   path = require("path"),
@@ -101,7 +117,7 @@ describe("testing " + testPN, function() {
     false
   );
 
-  
+
   var Bundle = require("../../lib/package/Bundle.js"),
     util = require("util"),
     bl = require("../../lib/package/bundleLinter.js");
