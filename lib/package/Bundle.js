apigee/apigeelint

commit 53b342901e6287a3f435cbde74ef1480cc895ac1

SharedFlow Bundle Support, fixing exclude options bug and adding EsLinter rule PO025 (#149)

lib/package/Bundle.js
@@ -24,6 +24,7 @@ var FindFolder = require("node-find-folder"),
   Endpoint = require("./Endpoint.js"),
   xpath = require("xpath"),
   Dom = require("xmldom").DOMParser,
+  bundleType = require('./BundleTypes.js'),
   debug = require("debug")("bundlelinter:Bundle");
 
 const util = require("util");
@@ -57,6 +58,16 @@ function _buildEndpoints(folder, tag, bundle, processFunction) {
 function buildProxyEndpoints(bundle) {
   var folder = bundle.proxyRoot + "/proxies/",
     tag = "ProxyEndpoint",
+    processFunction = function(element, fname, bundle) {
+      bundle.proxyEndpoints = bundle.proxyEndpoints || [];
+      bundle.proxyEndpoints.push(new Endpoint(element, bundle, fname));
+    };
+  _buildEndpoints(folder, tag, bundle, processFunction);
+}
+
+function buildsharedflows(bundle) {
+  var folder = bundle.proxyRoot + "/sharedflows/",
+    tag = "SharedFlow",
     processFunction = function(element, fname, bundle) {
       bundle.proxyEndpoints = bundle.proxyEndpoints || [];
       bundle.proxyEndpoints.push(new Endpoint(element, bundle, fname));
@@ -125,7 +136,7 @@ Bundle.prototype.getName = function() {
   if (!this.name) {
     //look at the root of the proxy for the .xml file and get name from there
     try {
-      var attr = xpath.select("/APIProxy/@name", this.getElement());
+      var attr = xpath.select("/" + this.xPathName + "/@name", this.getElement());
       this.name = (attr[0] && attr[0].value) || "undefined";
     } catch (e) {
       this.name = "undefined";
@@ -138,7 +149,7 @@ Bundle.prototype.getRevision = function() {
   if (!this.revision) {
     //look at the root of the proxy for the .xml file and get name from there
     try {
-      var attr = xpath.select("/APIProxy/@revision", this.getElement());
+      var attr = xpath.select("/" + this.xPathName + "/@revision", this.getElement());
       this.revision = (attr[0] && attr[0].value) || "undefined";
     } catch (e) {
       this.revision = "undefined";
@@ -147,11 +158,15 @@ Bundle.prototype.getRevision = function() {
   return this.revision;
 };
 
-function buildAll(bundle) {
+function buildAll(bundle, bundleName) {
   buildResources(bundle);
   buildPolicies(bundle);
   buildProxyEndpoints(bundle);
-  buildTargetEndpoints(bundle);
+
+  //no targets for shared flow
+  if(bundleName !== bundleType.BundleType.SHAREDFLOW){
+    buildTargetEndpoints(bundle);
+  }
 }
 
 function processFileSystem(config, bundle, cb) {
@@ -178,10 +193,13 @@ function processFileSystem(config, bundle, cb) {
   bundle.filePath = bundle.root + "/" + files[0];
 
   bundle.proxyRoot = bundle.root;
+
+  var bundleName = config.source.bundleType;
+
   //bundle.policies = [];
   //process.chdir(config.source.path);
   bundle.report = {
-    filePath: bundle.root.substring(bundle.root.indexOf("/apiproxy")),
+    filePath: bundle.root.substring(bundle.root.indexOf("/" + bundleName)),
     errorCount: 0,
     warningCount: 0,
     fixableErrorCount: 0,
@@ -189,21 +207,22 @@ function processFileSystem(config, bundle, cb) {
     messages: []
   };
 
-  if (!bundle.proxyRoot.endsWith("apiproxy")) {
-    var folders = new FindFolder("apiproxy");
-    //potentially have apiproxy and target/apiproxy
-    //we prefer the target bundle when we have both
-    //refactor to contains then walk through options
-    if (folders.includes("target/apiproxy")) {
+  //only run for proxy or shared flow root
+  if (!bundle.proxyRoot.endsWith(bundleName)) {
+    var folders = new FindFolder(bundleName);
+    //potentially have (apiproxy and target/apiproxy) or shared flow
+    if (bundleName == bundleType.BundleType.APIPROXY && folders.includes("target/apiproxy")) {
+      //we prefer the target bundle when we have both
+      //refactor to contains then walk through options
       bundle.proxyRoot = "target/apiproxy";
-      buildAll(bundle);
-    } else if (folders.includes("apiproxy")) {
-      bundle.proxyRoot = "apiproxy";
-      buildAll(bundle);
+      buildAll(bundle, bundleName);
+    } else if (folders.includes(bundleName)) {
+      bundle.proxyRoot = bundleName;
+      buildAll(bundle, bundleName);
     } else {
       //warn and be done
       bundle.addMessage({
-        message: "No apiproxy folder found in: " + JSON.stringify(folders)
+        message: "No " + bundleName + " folder found in: " + JSON.stringify(folders)
       });
     }
   }
@@ -218,9 +237,16 @@ function Bundle(config, cb) {
   //rewrite the source to be filesystem
   var bundle = this;
 
-  bundle.excluded = config.excluded;
-
+  this.xPathName = bundleType.getXPathName(config.source.bundleType);
+  this.bundleTypeName = config.source.bundleType;
+  this.excluded = config.excluded;
+  
   if (config.source.type === "ManagementServer") {
+    //shared flow not implemented for management server
+    if(config.source.bundleType === bundleTypes.BundleType.SHAREDFLOW){
+      throw "SharedFlows for management server not supported";
+    }
+
     var ManagementServer = require("./ManagementServer.js"),
       org = config.source.org,
       api = config.source.api,
@@ -286,7 +312,11 @@ Bundle.prototype.getReport = function(cb) {
         result.push(e.getReport());
       });
     };
-  append(this.getEndpoints());
+
+  //no endpoints in shared flow
+  if(!this.bundleTypeName == bundleType.BundleType.SHAREDFLOW){  
+    append(this.getEndpoints());
+  }
   append(this.getPolicies());
   append(this.getResources());
   if (cb) {
@@ -567,7 +597,12 @@ Bundle.prototype.onTargetEndpoints = function(pluginFunction, cb) {
 
 Bundle.prototype.getProxyEndpoints = function() {
   if (!this.proxyEndpoints) {
-    buildProxyEndpoints(this);
+    if(this.bundleTypeName === bundleType.BundleType.SHAREDFLOW){
+      buildsharedflows(this);
+    }else {
+      buildProxyEndpoints(this);
+    }
+
     if (!this.proxyEndpoints) {
       this.proxyEndpoints = [];
     }
@@ -588,7 +623,11 @@ Bundle.prototype.getTargetEndpoints = function() {
 Bundle.prototype.getEndpoints = function() {
   if (!this.endpoints) {
     this.endpoints = this.getProxyEndpoints();
-    this.endpoints = this.endpoints.concat(this.getTargetEndpoints());
+
+    //no targets for shared flows
+    if(this.bundleTypeName !== bundleType.BundleType.SHAREDFLOW){
+      this.endpoints = this.endpoints.concat(this.getTargetEndpoints());
+    }
   }
   return this.endpoints;
 };

cli.js
@@ -19,6 +19,7 @@
 var bl = require("./lib/package/bundleLinter.js");
 var program = require("commander");
 var pkj = require('./package.json');
+var bundleType = require('./lib/package/BundleTypes.js')
 
 program
   .version(pkj.version)
@@ -47,7 +48,8 @@ var configuration = {
   debug: true,
   source: {
     type: "filesystem",
-    path: program.path
+    path: program.path,
+    bundleType: program.path.includes(bundleType.BundleType.SHAREDFLOW) ? bundleType.BundleType.SHAREDFLOW : bundleType.BundleType.APIPROXY
   },
   excluded: {}
 };
@@ -66,6 +68,7 @@ if (program.excluded && typeof(program.excluded) === "string") {
 }
 
 if (program.user) {
+  //check for required fields
   configuration.apiUpload = {
     destPath:
       program.destPath || "https://csdata-test.apigee.net/v1/lintresults",

lib/package/BundleTypes.js
@@ -1,0 +1,13 @@
+const BundleType = {
+    APIPROXY: "apiproxy",
+    SHAREDFLOW: "sharedflowbundle"
+}
+
+var getXPathName = function(bundleType){
+    return bundleType === BundleType.SHAREDFLOW ? "SharedFlowBundle" : "APIProxy";
+}
+
+module.exports = {
+ BundleType,
+ getXPathName
+}

lib/package/Endpoint.js
@@ -23,12 +23,13 @@ var Flow = require("./Flow.js"),
   debug = require("debug")("bundlelinter:Endpoint"),
   fs = require("fs");
 
-function Endpoint(element, parent, fname) {
+function Endpoint(element, parent, fname, bundletype) {
+  this.bundleType = bundletype ? bundletype : "apiproxy" //default to apiproxy if bundletype not passed
   this.fileName = fname;
   this.parent = parent;
   this.element = element;
   this.report = {
-    filePath: fname.substring(fname.indexOf("/apiproxy")),
+    filePath: fname.substring(fname.indexOf("/" + this.bundleType)),
     errorCount: 0,
     warningCount: 0,
     fixableErrorCount: 0,
@@ -145,6 +146,17 @@ Endpoint.prototype.getPostClientFlow = function() {
   return this.postClientFlow;
 };
 
+Endpoint.prototype.getSharedFlow = function() {
+  if (!this.sharedFlow) {
+    //find the preflow tag
+    var doc = xpath.select(".", this.element);
+    if (doc && doc[0]) {
+      this.sharedFlow = new Flow(doc[0], this);
+    }
+  }
+  return this.sharedFlow;
+};
+
 Endpoint.prototype.getRouteRules = function() {
   if (!this.routeRules) {
     var doc = xpath.select("./RouteRule", this.element),
@@ -184,9 +196,15 @@ Endpoint.prototype.getAllFlows = function() {
   if (!this.allFlows) {
     self = this;
     this.allFlows = [];
-    this.allFlows.push(this.getPreFlow());
-    this.allFlows.push(this.getPostFlow());
-	this.allFlows.push(this.getPostClientFlow());
+
+    if(this.bundleType === "SharedFlow"){
+      this.allFlows.push(this.getSharedFlow());
+    } else {
+      this.allFlows.push(this.getPreFlow());
+      this.allFlows.push(this.getPostFlow());
+      this.allFlows.push(this.getPostClientFlow());
+    }
+
     var flows = [];
     flows = this.getFlows();
     flows.forEach(function(flow) {

lib/package/Flow.js
@@ -98,6 +98,16 @@ Flow.prototype.getFlowResponse = function() {
   return this.flowResponse;
 };
 
+Flow.prototype.getSharedFlow = function() {
+  if (!this.flowResponse) {
+    var doc = xpath.select(".", this.element);
+    if (doc && doc[0]) {
+      this.flowResponse = new FlowPhase(doc[0], this);
+    }
+  }
+  return this.flowResponse;
+};
+
 Flow.prototype.onSteps = function(pluginFunction, callback) {
   //parallel these
   var flow = this;

lib/package/plugins/checkBundleStructure.js
@@ -154,8 +154,9 @@ function checkNode(node, curRoot) {
   });
 }
 
-var bundleStructure = {
-    name: "apiproxy",
+var bundleStructure = function (bundleType) {
+  var structure = {
+    name: bundleType,
     files: { extensions: ["xml", "md"], maxCount: 1 },
     folders: [
 	  { name: "manifests", required: false, files: { extensions: ["xml"] } },
@@ -165,12 +166,6 @@ var bundleStructure = {
         required: false,
         files: { extensions: ["xml"] }
       },
-      {
-        name: "proxies",
-        required: true,
-        files: { extensions: ["xml", "flowfrag"] }
-      },
-      { name: "targets", required: false, files: { extensions: ["xml"] } },
       {
         name: "resources",
         required: false,
@@ -250,15 +245,44 @@ var bundleStructure = {
         ]
       }
     ]
-  },
-  onBundle = function(b, cb) {
-    bundle = b;
-    root = bundle.proxyRoot;
-    checkNode(bundleStructure);
-    if (typeof cb == "function") {
-      cb(null, hadWarnErr);
+  }
+
+  if (bundleType == "apiproxy") {
+    var proxy = {
+      name: "proxies",
+      required: true,
+      files: { extensions: ["xml", "flowfrag"] }
     }
-  };
+
+    var targets = { name: "targets", required: false, files: { extensions: ["xml"] } }
+
+    structure.folders.push(proxy);
+    structure.folders.push(targets);
+  }
+
+  if(bundleType == "sharedflowbundle"){
+    var flow = {
+      name: "sharedflows",
+      required: true,
+      files: { extensions: ["xml"] }
+    }
+
+    structure.folders.push(flow);
+  }
+
+  return structure;
+}
+
+var onBundle = function (b, cb) {
+  bundle = b;
+  root = bundle.proxyRoot;
+  var bundleStruct = bundleStructure(bundle.bundleTypeName)
+
+  checkNode(bundleStruct);
+  if (typeof cb == "function") {
+    cb(null, hadWarnErr);
+  }
+}
 
 module.exports = {
   plugin,

lib/package/plugins/eslint.js
@@ -1,0 +1,57 @@
+var plugin = {
+    ruleId: "PO025",
+    name: "esLint",
+    message: "esLint applied to all javascript resource callouts.",
+    fatal: false,
+    severity: 1,
+    nodeType: "Resource",
+    enabled: true
+  },
+  debug = require("debug")("bundlelinter:" + plugin.name), hadWarnErr=false;
+
+var onResource = function(resource, cb) {
+  try {
+    var fname = resource.getFileName();
+    if (
+      fname.endsWith(".jsc") ||
+      fname.endsWith(".js") ||
+      fname.endsWith(".json")
+    ) {
+      const Linter = require("eslint"),
+        linter = new Linter.Linter();
+        
+      const messages = linter.verify(resource.getContents(), {
+          rules: {
+              semi: 2
+          }
+      }, { filename: fname });
+
+      if(messages.length > 0){
+        messages.forEach(function(error){
+          plugin.severity = error.severity;
+          
+          var result = {
+            plugin,
+            source: error.fix.text,
+            line: error.line,
+            column: error.column,
+            message: error.ruleId + ": " + error.nodeType + ": " + error.message
+          };
+          resource.addMessage(result);
+          hadWarnErr=true;
+        });
+      }
+    }
+  } catch (e) {
+    debugger;
+    debug("eslint error" + e);
+  }
+  if (typeof cb == "function") {
+    cb(null, hadWarnErr);
+  }
+};
+
+module.exports = {
+  plugin,
+  onResource
+};

test/fixtures/resources/sampleFlow/24Solver/sharedflowbundle/TwentyFour.xml
@@ -1,4 +1,4 @@
-/*
+<!--
   Copyright 2019 Google LLC
 
   Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,26 +12,10 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-*/
+ -->
 
-configuration = {
-  debug: false,
-  source: {
-    type: "filesystem",
-    path: "./test/fixtures/resources/sampleProxy/24Solver/apiproxy",
-    bundleType: "apiproxy"
-  },
-  excluded: {}
-};
-
-
-
-sharedflowconfiguration = {
-  debug: false,
-  source: {
-    type: "filesystem",
-    path: "./test/fixtures/resources/sampleFlow/24Solver/sharedflowbundle",
-    bundleType: "sharedflowbundle"
-  },
-  excluded: {}
-};
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<SharedFlowBundle revision="1" name="TwentyFourFlow">
+    <Description>TwentyFourFlow shared flow testing</Description>
+    <DisplayName>TwentyFourFlow</DisplayName>
+</SharedFlowBundle>

commit 1c7251d8da9321b13e6623bec374c81344eee8ad

Fixed the if condition that prevented the plugins for Endpoints to correctly generate the report object. The bug was introduced in 53b3429

lib/package/Bundle.js
@@ -314,7 +314,7 @@ Bundle.prototype.getReport = function(cb) {
     };
 
   //no endpoints in shared flow
-  if(!this.bundleTypeName == bundleType.BundleType.SHAREDFLOW){  
+  if(this.bundleTypeName !== bundleType.BundleType.SHAREDFLOW){  
     append(this.getEndpoints());
   }
   append(this.getPolicies());