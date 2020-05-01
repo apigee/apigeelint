341f29a258ade85dfadb03b5a5133419fe8e227c
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2020-05-01 10:58:22 -0400

    Fixed issue #161, TD002 incorrect report

---
+++ test/specs/testuseTargetServers.js
@@ -0,0 +1,119 @@
+/*
+  Copyright 2019 Google LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+var assert = require("assert"),
+  decache = require("decache"),
+  path = require("path"),
+  fs = require("fs"),
+  testPN = "useTargetServers.js",
+  debug = require("debug")("bundlelinter:" + testPN),
+  Bundle = require("../../lib/package/Bundle.js"),
+  util = require("util"),
+  bl = require("../../lib/package/bundleLinter.js");
+
+var Endpoint = require("../../lib/package/Endpoint.js"),
+  plugin = require("../../lib/package/plugins/" + testPN),
+  Dom = require("xmldom").DOMParser,
+  test = function(targetDef, assertion) {
+    it(
+      "testing " + testPN + '" expected to see ' + assertion + ".",
+      function() {
+        var tDoc = new Dom().parseFromString(targetDef),
+          target = new Endpoint(tDoc, this, "");
+
+        target.addMessage = function(msg) {
+          debug(msg);
+        };
+
+        plugin.onTargetEndpoint(target, function(err, result) {
+          assert.equal(err, undefined, err ? " err " : " no err");
+          assert.equal(
+            result,
+            assertion,
+            result
+              ? "warning/error was returned"
+              : "warning/error was not returned"
+          );
+        });
+      }
+    );
+  };
+
+//now generate a full report and check the format of the report
+
+
+describe("testing " + testPN, function() {
+
+
+  test(
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+      <URL>https://foo.com/apis/{api_name}/maskconfigs</URL>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    true
+  );
+
+  test(
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+        <LoadBalancer>
+            <Server name="server1"/>
+        </LoadBalancer>
+        <Path>{flow.target.basepath}/{flow.target.pathsuffix}</Path>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    false
+  );
+
+  var Bundle = require("../../lib/package/Bundle.js"),
+    util = require("util"),
+    bl = require("../../lib/package/bundleLinter.js");
+
+  debug("test configuration: " + JSON.stringify(configuration));
+
+  var bundle = new Bundle(configuration);
+  bl.executePlugin(testPN, bundle);
+
+  describe("Print " + testPN + " plugin results", function() {
+    var report = bundle.getReport(),
+      jsimpl = bl.getFormatter("json.js");
+
+    if (!jsimpl) {
+      assert("implementation not defined: " + jsimpl);
+    } else {
+      it("should create a report object with valid schema", function() {
+        var schema = require("./../fixtures/reportSchema.js"),
+          Validator = require("jsonschema").Validator,
+          v = new Validator(),
+          validationResult,
+          jsonReport;
+
+        var jsonReport = JSON.parse(jsimpl(bundle.getReport()));
+        validationResult = v.validate(jsonReport, schema);
+        assert.equal(
+          validationResult.errors.length,
+          0,
+          validationResult.errors
+        );
+      });
+    }
+  });
+
+  var stylimpl = bl.getFormatter("unix.js");
+  var stylReport = stylimpl(bundle.getReport());
+  debug("unix formatted report: \n" + stylReport);
+});
