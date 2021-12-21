462b7559b6d8c95f12ea67ccc7097186f3fe3ef5
Dino Chiesa <dchiesa@google.com>
2021-12-20 18:03:53 -0800

    increase code coverage with tests - no new function
    adds new tests for formatters, HMAC policy, and Step hygiene.
    
    also remove some unused dependencies: decache, request, util

---
+++ test/fixtures/resources/PO027-hmac/fail/messages.js
@@ -0,0 +1,10 @@
+// These are the error messages only for the failed policies.
+module.exports = {
+  "HMAC-Fail-1.xml" : "Algorithm name value should not have leading/trailing whitespace",
+  "HMAC-Fail-2.xml" : "Missing Algorithm element",
+  "HMAC-Fail-3.xml" : "More than one Algorithm element",
+  "HMAC-Fail-4.xml" : "\"private.\" is the only supported prefix for SecretKey ref attribute.",
+  "HMAC-Fail-5.xml" : "empty ref= attribute on SecretKey",
+  "HMAC-Fail-6.xml" : "SecretKey must use a ref= attribute",
+  "HMAC-Fail-7.xml" : "Missing SecretKey element"
+};
