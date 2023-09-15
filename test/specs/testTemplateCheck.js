0e63688e60c72bdbf6a51a7cfd73470cf86031f2
Dino Chiesa <dchiesa@google.com>
2023-09-15 10:53:53 -0700

    fix: validate function names and basic args

--- test/specs/testTemplateCheck.js
+++ test/specs/testTemplateCheck.js
@@ -21,46 +21,59 @@ const assert = require("assert"),
 
 describe("TemplateCheck", function() {
   const testCases = [
-          ["{}", false],
-          ["{a}", true],
-          ["{{a}", false],
-          ["{{a}}", false],
-          ["{[]}", false],
-          ["{a[]}", false],
-          ["{a", false],
-          ["}a", false],
-          ["{a}}", false],
-          ["}a{", false],
-          ["{a}b", true],
-          ["{a}{b}", true],
-          ["{timeFormatUTCMs(propertyset.set1.timeformat,system.timestamp)}", true],
-          ["{timeFormatUTCMs(propertyset.set1.timeformat,system.timestamp) }", false],
-          [`{  "organization": "{organization.name}", "environment": "{environment.name}" } `, true],
-          [`{  "organization": "{organization.name}", "environment": "{environment.{name}}" } `, false],
-          [`{  "organization": "{organization.name}", "other": {"environment": "{environment.name}" } } `, true],
+          ["{}", "empty function name at position 1"],
+          ["{a}", undefined],
+          ["{{a}", 'unexpected character at position 1: {'],
+          ["{{a}}", 'unexpected character at position 1: {'],
+          ["{[]}", 'unexpected character at position 1: ['],
+          ["{a[]}", 'unexpected character at position 2: ['],
+          ["{a", 'unterminated curly brace'],
+          ["}a", 'unexpected close curly at position 0'],
+          ["{a}}", 'unexpected close curly at position 3'],
+          ["}a{", 'unexpected close curly at position 0'],
+          ["{a}b", undefined],
+          ["{a}{b}", undefined],
+          ["{a}.{b}", undefined],
+          ["{a.b}", undefined],
+          ["{3}", 'unexpected character at position 1: 3'],
+          ["{timeFormatUTCMs(propertyset.set1.timeformat,system.timestamp)}", undefined],
+          ["{timeFormatUTCMs(propertyset.set1.timeformat,system.timestamp) }",
+           'unexpected character at position 62:  '],
+          [`{  "organization": "{organization.name}", "environment": "{environment.name}" } `, undefined],
+          [`{  "organization": "{organization.name}", "environment": "{environment.{name}}" } `,
+           'unexpected character at position 71: {'],
+          [`{  "organization": "{organization.name}", "other": {"environment": "{environment.name}" } } `,
+           undefined],
+          ["{createUuid()}", undefined],
+          ["{createUuid( )}", 'spaces in argument string for function createUuid'],
+          ["{timeFormatUTCMs()}", 'empty arg string for function timeFormatUTCMs'],
+          ["{notARealfunction()}", 'unsupported function name (notARealfunction)'],
+          ["{createUuid[]}", 'unexpected character at position 11: ['],
+          ["{ createUuid() }", undefined], // but ineffective
         ];
 
   testCases.forEach( (item, _ix) => {
-    const [expression, expectedResult] = item;
-    it(`Check template (${expression}) should return ${expectedResult}`, function() {
-      const actualResult = pu.isValidTemplate(expression);
-      assert.equal(actualResult, expectedResult);
+    const [expression, expectedError] = item;
+    it(`validateTemplate (${expression}) should return ${expectedError}`, function() {
+      const actualResult = pu.validateTemplate(expression);
+      assert.equal(actualResult, expectedError);
     });
   });
 });
 
 describe("PropertySetRefCheck", function() {
   const testCases = [
-          ["{foo.bar}", true],
-          ["{foo.bar}.{var2}.setting", false],
-          ["{foo.bar}.setting", true],
-          ["{foo.bar}.{flow.var2}", true],
+          ["{foo.bar}", undefined],
+          ["{foo.bar}.{var2}.setting", 'there is more than one dot in the template result'],
+          ["foo.bar.var2.setting", 'there are 3 dots ( !=1 ) in the template result'],
+          ["{foo.bar}.setting", undefined],
+          ["{foo.bar}.{flow.var2}", undefined],
         ];
 
   testCases.forEach( (item, _ix) => {
     const [expression, expectedResult] = item;
     it(`PropertySetRef (${expression}) should be ${expectedResult?'valid':'not valid'}`, function() {
-      const actualResult = pu.isValidPropertySetRef(expression);
+      const actualResult = pu.validatePropertySetRef(expression);
       assert.equal(actualResult, expectedResult);
     });
   });
