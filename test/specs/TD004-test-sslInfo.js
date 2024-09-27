be3937433f716184d621ad1d2cc8aaa12d80e08f
Dino Chiesa <dchiesa@google.com>
2024-09-27 16:14:53 -0700

    chore: refactor TD004 into multiple different plugins

---
+++ test/specs/TD004-test-sslInfo.js
@@ -0,0 +1,212 @@
+/*
+  Copyright 2019-2024 Google LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+/* global describe, it, configuration */
+
+const assert = require("assert"),
+  testID = "TD004",
+  util = require("util"),
+  debug = require("debug")("apigeelint:" + testID),
+  Bundle = require("../../lib/package/Bundle.js"),
+  bl = require("../../lib/package/bundleLinter.js"),
+  Endpoint = require("../../lib/package/Endpoint.js"),
+  plugin = require(bl.resolvePlugin(testID)),
+  Dom = require("@xmldom/xmldom").DOMParser,
+  testBase = function (caseNum, profile, desc, targetDef, messages) {
+    it(`case ${caseNum} ${desc}`, function () {
+      const tDoc = new Dom().parseFromString(targetDef),
+        target = new Endpoint(tDoc.documentElement, this, "");
+
+      plugin.onBundle({ profile });
+
+      plugin.onTargetEndpoint(target, function (e, result) {
+        assert.equal(e, undefined, e ? " error " : " no error");
+        debug(`result: ${result}`);
+        if (messages && messages.length) {
+          debug(`messages: ${util.format(messages)}`);
+          assert.equal(result, true);
+          assert.equal(
+            target.report.messages.length,
+            messages.length,
+            util.format(target.report.messages)
+          );
+          messages.forEach((msg, ix) => {
+            debug(`check msg ${ix}: ${msg}`);
+            assert.ok(
+              target.report.messages.find((m) => m.message == msg),
+              `index ${ix} ${util.format(target.report.messages)}`
+            );
+          });
+        } else {
+          assert.equal(result, false, util.format(target.report.messages));
+          assert.equal(
+            target.report.messages.length,
+            0,
+            util.format(target.report.messages)
+          );
+        }
+      });
+    });
+  };
+
+const test = function (caseNum, desc, targetDef, messages) {
+  return testBase(caseNum, "apigee", desc, targetDef, messages);
+};
+const testApigeeX = function (caseNum, desc, targetDef, messages) {
+  return testBase(caseNum, "apigeex", desc, targetDef, messages);
+};
+
+describe(`${testID} - ${plugin.plugin.name}`, function () {
+  test(
+    20,
+    "empty SSLInfo with https url",
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+      <SSLInfo/>
+      <URL>https://foo.com/apis/{api_name}/maskconfigs</URL>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    ["SSLInfo configuration does not use Enabled=true"]
+  );
+
+  test(
+    30,
+    "SSLInfo Enabled = false",
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+      <SSLInfo>
+        <Enabled>false</Enabled>
+      </SSLInfo>
+      <URL>https://foo.com/apis/{api_name}/maskconfigs</URL>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    ["SSLInfo configuration does not use Enabled=true"]
+  );
+
+  test(
+    40,
+    "SSLInfo Enabled = true, no truststore",
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+      <SSLInfo>
+        <Enabled>true</Enabled>
+      </SSLInfo>
+      <URL>https://foo.com/apis/{api_name}/maskconfigs</URL>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    []
+  );
+
+  test(
+    41,
+    "SSLInfo Enabled = true, with TrustStore",
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+      <SSLInfo>
+        <Enabled>true</Enabled>
+        <TrustStore>truststore1</TrustStore>
+      </SSLInfo>
+      <URL>https://foo.com/apis/{api_name}/maskconfigs</URL>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    null
+  );
+
+  // test(
+  //   43,
+  //   "SSLInfo Enabled = true, with LoadBalancer",
+  //   `<TargetEndpoint name="default">
+  //   <HTTPTargetConnection>
+  //     <SSLInfo>
+  //       <Enabled>true</Enabled>
+  //       <IgnoreValidationErrors>false</IgnoreValidationErrors>
+  //       <TrustStore>truststore1</TrustStore>
+  //     </SSLInfo>
+  //     <LoadBalancer>
+  //        <Server name="server1"/>
+  //        <Server name="server2"/>
+  //     </LoadBalancer>
+  //   </HTTPTargetConnection>
+  // </TargetEndpoint>`,
+  //   []
+  // );
+
+  testApigeeX(
+    90,
+    "SSLInfo with Enforce",
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+      <SSLInfo>
+        <Enabled>true</Enabled>
+        <Enforce>true</Enforce>
+        <TrustStore>truststore1</TrustStore>
+      </SSLInfo>
+      <URL>https://foo.com/apis/{api_name}/maskconfigs</URL>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    null
+  );
+
+  testApigeeX(
+    91,
+    "SSLInfo with Enforce",
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+      <SSLInfo>
+        <Enabled>true</Enabled>
+        <TrustStore>truststore1</TrustStore>
+      </SSLInfo>
+      <URL>https://foo.com/apis/{api_name}/maskconfigs</URL>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    ["SSLInfo configuration does not use Enforce=true"]
+  );
+
+  test(
+    92,
+    "SSLInfo with Enforce - not ApigeeX",
+    `<TargetEndpoint name="default">
+    <HTTPTargetConnection>
+      <SSLInfo>
+        <Enabled>true</Enabled>
+        <Enforce>true</Enforce>
+        <TrustStore>truststore1</TrustStore>
+      </SSLInfo>
+      <URL>https://foo.com/apis/{api_name}/maskconfigs</URL>
+    </HTTPTargetConnection>
+  </TargetEndpoint>`,
+    ["SSLInfo configuration must not use the Enforce element"]
+  );
+});
+
+describe(`${testID} - Print plugin results`, function () {
+  debug("test configuration: " + JSON.stringify(configuration));
+  var bundle = new Bundle(configuration);
+  bl.executePlugin(testID, bundle);
+  let report = bundle.getReport();
+
+  it("should create a report object with valid schema", function () {
+    let formatter = bl.getFormatter("json.js");
+    if (!formatter) {
+      assert.fail("formatter implementation not defined");
+    }
+    let schema = require("./../fixtures/reportSchema.js"),
+      Validator = require("jsonschema").Validator,
+      v = new Validator(),
+      jsonReport = JSON.parse(formatter(report)),
+      validationResult = v.validate(jsonReport, schema);
+    assert.equal(validationResult.errors.length, 0, validationResult.errors);
+  });
+});
---
+++ test/specs/TD013-test.js
@@ -0,0 +1,93 @@
+/*
+  Copyright 2019-2024 Google LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+/* global describe, it */
+
+const testID = "TD013",
+  assert = require("assert"),
+  fs = require("fs"),
+  util = require("util"),
+  path = require("path"),
+  bl = require("../../lib/package/bundleLinter.js"),
+  plugin = require(bl.resolvePlugin(testID)),
+  Endpoint = require("../../lib/package/Endpoint.js"),
+  Dom = require("@xmldom/xmldom").DOMParser,
+  rootDir = path.resolve(__dirname, "../fixtures/resources/TD013"),
+  debug = require("debug")("apigeelint:" + testID);
+
+const loadEndpoint = (sourceDir, shortFileName) => {
+  const fqPath = path.join(sourceDir, shortFileName),
+    xml = fs.readFileSync(fqPath).toString("utf-8"),
+    doc = new Dom().parseFromString(xml),
+    endpoint = new Endpoint(doc.documentElement, null, "");
+  endpoint.getFileName = () => shortFileName;
+  return endpoint;
+};
+
+describe(`${testID} - endpoint passes ClientAuthEnabled check`, function () {
+  const sourceDir = path.join(rootDir, "pass");
+  const testOne = (shortFileName) => {
+    const endpoint = loadEndpoint(sourceDir, shortFileName);
+
+    it(`check ${shortFileName} passes`, () => {
+      plugin.onTargetEndpoint(endpoint, (e, foundIssues) => {
+        assert.equal(e, undefined, "should be undefined");
+        const messages = endpoint.getReport().messages;
+        debug(util.format(messages));
+        assert.equal(foundIssues, false, "should be no issues");
+        assert.ok(messages, "messages should exist");
+        assert.equal(messages.length, 0, "unexpected number of messages");
+      });
+    });
+  };
+
+  const candidates = fs
+    .readdirSync(sourceDir)
+    .filter((shortFileName) => shortFileName.endsWith(".xml"));
+
+  it(`checks that there are tests`, () => {
+    assert.ok(candidates.length > 0, "tests should exist");
+  });
+
+  candidates.forEach(testOne);
+});
+
+describe(`${testID} - endpoint does not pass ClientAuthEnabled check`, () => {
+  const sourceDir = path.join(rootDir, "fail");
+
+  const testOne = (shortFileName) => {
+    const policy = loadEndpoint(sourceDir, shortFileName);
+    it(`check ${shortFileName} throws error`, () => {
+      plugin.onTargetEndpoint(policy, (e, foundIssues) => {
+        assert.equal(undefined, e, "should be undefined");
+        assert.equal(true, foundIssues, "should be issues");
+        const messages = policy.getReport().messages;
+        assert.ok(messages, "messages for issues should exist");
+        debug(util.format(messages));
+      });
+    });
+  };
+
+  const candidates = fs
+    .readdirSync(sourceDir)
+    .filter((shortFileName) => shortFileName.endsWith(".xml"));
+
+  it(`checks that there are tests`, () => {
+    assert.ok(candidates.length > 0, "tests should exist");
+  });
+
+  candidates.forEach(testOne);
+});
