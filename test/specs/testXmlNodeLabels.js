beb7609cb5c649e6b5e0313a8897ce790d1e8587
DinoChiesa <dpchiesa@hotmail.com>
2025-10-19 14:18:24 -0700

    fix: extend a few test timeouts to allow success on slower machines

--- test/specs/testXmlNodeLabels.js
+++ test/specs/testXmlNodeLabels.js
@@ -1,5 +1,5 @@
-/*
-  Copyright 2024 Google LLC
+﻿/*
+  Copyright © 2024-2025 Google LLC
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -60,7 +60,7 @@ describe("xmldom path resolution verification", function () {
   });
 
   it("should find xmldom", function (done) {
-    this.timeout(38000);
+    this.timeout(58000);
     const opts = {
       cwd: proxyDir,
       encoding: "utf8",
