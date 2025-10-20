be71ac27801ba5cc199e7f27cf8e9d40ef0b7f23
DinoChiesa <dpchiesa@hotmail.com>
2025-10-20 08:01:28 -0700

    wip: intial tests for issue 575

--- test/specs/CC005-test.js
+++ test/specs/CC005-test.js
@@ -1,5 +1,5 @@
-/*
-  Copyright 2019-2021,2025 Google LLC
+﻿/*
+  Copyright © 2019-2021,2025 Google LLC
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -64,6 +64,11 @@ describe(`${testID} - malformed Conditions`, () => {
         },
         {
           line: 59,
+          column: 7,
+          message: "Invalid Condition - cannot parse",
+        },
+        {
+          line: 67,
           column: 7,
           message: "Invalid Condition - cannot parse",
         },
