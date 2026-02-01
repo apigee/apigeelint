b24525bf488337c333969c0a811f70409ba299d7
DinoChiesa <dpchiesa@hotmail.com>
2026-02-01 12:35:50 -0800

    test: add cases for issue594, variables containing dots

--- test/specs/ST003-ev-json-condition.js
+++ test/specs/ST003-ev-json-condition.js
@@ -1,5 +1,5 @@
-/*
-  Copyright 2019-2022 Google LLC
+﻿/*
+Copyright © 2019-2022,2026 Google LLC
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -17,76 +17,87 @@
 /* global describe, it */
 
 const assert = require("assert"),
-      ruleId = 'ST003',
-      path = require("path"),
-      debug = require("debug")(`apigeelint:${ruleId}`),
-      util = require("util"),
-      bl = require("../../lib/package/bundleLinter.js");
+  ruleId = "ST003",
+  path = require("path"),
+  debug = require("debug")(`apigeelint:${ruleId}`),
+  util = require("util"),
+  bl = require("../../lib/package/bundleLinter.js");
 
-const expectedMessageRe =
-  new RegExp("^For the ExtractVariables step \\([-A-Za-z0-9_]{2,}\\), an appropriate check for a message body was not found\\..*");
+const expectedMessageRe = new RegExp(
+  "^For the ExtractVariables step \\([-A-Za-z0-9_]{2,}\\), an appropriate check for a message body was not found\\..*",
+);
 
 describe(`${ruleId} - ExtractVariables JSONPayload Conditions`, () => {
   function check(suffix, bundleType, expected) {
     let configuration = {
-          debug: true,
-          source: {
-            type: "filesystem",
-            path: path.resolve(__dirname, `../fixtures/resources/ExtractVariables-Attachment/${bundleType}`),
-            bundleType
-          },
-          excluded: {},
-          setExitCode: false,
-          output: () => {} // suppress output
-        };
+      debug: true,
+      source: {
+        type: "filesystem",
+        path: path.resolve(
+          __dirname,
+          `../fixtures/resources/ExtractVariables-Attachment/${bundleType}`,
+        ),
+        bundleType,
+      },
+      excluded: {},
+      setExitCode: false,
+      output: () => {}, // suppress output
+    };
 
     bl.lint(configuration, (bundle) => {
       let items = bundle.getReport();
       assert.ok(items);
       assert.ok(items.length);
       debug(util.format(items));
-      let sfItems = items.filter( m => m.filePath.endsWith(suffix));
+      let sfItems = items.filter((m) => m.filePath.endsWith(suffix));
       debug(util.format(sfItems));
       assert.equal(sfItems.length, 1);
-      sfItems.forEach( item =>
-                       debug(util.format(item.messages)));
-      let st003Messages = sfItems[0].messages.filter( m => m.ruleId == ruleId);
+      sfItems.forEach((item) => debug(util.format(item.messages)));
+      let st003Messages = sfItems[0].messages.filter((m) => m.ruleId == ruleId);
       debug(util.format(st003Messages));
       assert.equal(st003Messages.length, expected.length);
 
-      expected.forEach( (item, ix) => {
+      expected.forEach((item, ix) => {
         assert.equal(st003Messages[ix].line, item.line, `case(${ix}) line`);
-        assert.equal(st003Messages[ix].column, item.column, `case(${ix}) column`);
+        assert.equal(
+          st003Messages[ix].column,
+          item.column,
+          `case(${ix}) column`,
+        );
         assert.equal(st003Messages[ix].severity, 1, `case(${ix}) severity`);
-        assert.ok(st003Messages[ix].message.match(expectedMessageRe), `case(${ix}) message`);
+        assert.ok(
+          st003Messages[ix].message.match(expectedMessageRe),
+          `case(${ix}) message`,
+        );
       });
     });
   }
 
-  it('should find the expected warnings in an apiproxy', () => {
+  it("should find the expected warnings in an apiproxy", () => {
     let expected = [
-          {
-            line: 100,
-            column: 9
-          },
-          {
-            line: 112,
-            column: 9
-          },
-          {
-            line: 125,
-            column: 9
-          }
-        ];
+      {
+        line: 100,
+        column: 9,
+      },
+      {
+        line: 112,
+        column: 9,
+      },
+      {
+        line: 125,
+        column: 9,
+      },
+      {
+        line: 180,
+        column: 9,
+      },
+    ];
 
-    check('endpoint1.xml', 'apiproxy', expected);
+    check("endpoint1.xml", "apiproxy", expected);
   });
 
-  it('should find the expected warnings in a sharedflow', () => {
-    let expected = [
-          { line: 5, column: 3 }
-        ];
-    check('sf-default.xml', 'sharedflowbundle', expected);
+  it("should find the expected warnings in a sharedflow", () => {
+    let expected = [{ line: 5, column: 3 }];
+    check("sf-default.xml", "sharedflowbundle", expected);
   });
-
 });
