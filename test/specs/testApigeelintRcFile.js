038da7823d1e6912f559673e06ce9053cc206452
DinoChiesa <dpchiesa@hotmail.com>
2026-02-28 16:57:57 -0800

    fix: correct test logic for cli and exit codes
    This change also allows the tests to run correctly on Windows.

--- test/specs/testApigeelintRcFile.js
+++ test/specs/testApigeelintRcFile.js
@@ -18,52 +18,70 @@ Copyright © 2026 Google LLC
 
 const assert = require("node:assert"),
   debug = require("debug")(`apigeelint:rc-test`),
+  tmp = require("tmp"),
   child_process = require("node:child_process");
 
 describe("cli options handling", function () {
-  this.slow(11000);
+  this.slow(61000);
+  this.timeout(8000);
   const path = require("node:path"),
     fs = require("node:fs"),
-    proxyDir = path.resolve(__dirname, "../fixtures/resources/issue608"),
-    node_modules = path.resolve(proxyDir, "node_modules"),
-    packageLock = path.resolve(proxyDir, "package-lock.json");
+    issue608Dir = path.resolve(__dirname, "../fixtures/resources/issue608"),
+    tmpdir = tmp.dirSync({
+      prefix: `apigeelint-cli-test`,
+      keep: false,
+      unsafeCleanup: true, // this does not seem to work in apigeelint
+    });
 
-  const cleanup = (done) => {
-    fs.rmSync(node_modules, { force: true, recursive: true });
-    fs.rmSync(packageLock, { force: true });
-    done();
-  };
-  before(function (done) {
-    // remove node_modules before the test runs. We want a clean install.
-    cleanup(done);
+  // make sure to cleanup when the process exits
+  process.on("exit", function () {
+    tmpdir.removeCallback();
   });
 
-  after(function (done) {
-    // tidy up after the test runs.
-    this.timeout(8000);
-    // Sometimes this gets hung and fails. It seems it's a race condition.
-    // Trying a timeout to avoid that.
-    setTimeout(() => cleanup(done), 2000);
-  });
   const spawnOpts = {
-    cwd: proxyDir,
+    cwd: tmpdir.name,
     encoding: "utf8",
-    env: { ...process.env, DEBUG: "apigeelint:rc" },
+    env: { ...process.env, DEBUG: "apigeelint:cli,apigeelint:rc" },
   };
 
   const runOne = (cliOpts, checkCb) => {
-    //  npm install can take a very long time, sometimes.
+    fs.cpSync(
+      path.resolve(issue608Dir, "package.json"),
+      path.resolve(tmpdir.name, "package.json"),
+      { force: true },
+    );
+    fs.cpSync(
+      path.resolve(issue608Dir, ".apigeelintrc"),
+      path.resolve(tmpdir.name, ".apigeelintrc"),
+      { force: true },
+    );
+
+    // npm install can take a very long time, sometimes.
+    // NB: multiple tests result in re-install of apigeelint in the same directory.
     child_process.exec("npm install", spawnOpts, (e, stdout, stderr) => {
       assert.equal(e, null);
       debug(stdout);
       // get the current cli.js, to allow testing of it.
       const srcCli = path.resolve(__dirname, "../../cli.js"),
-        destCli = path.resolve(node_modules, "apigeelint/cli.js");
+        destCli = path.resolve(tmpdir.name, "node_modules/apigeelint/cli.js");
       fs.cpSync(srcCli, destCli, { force: true });
+      // and bundleLinter too
+      const srcBl = path.resolve(
+          __dirname,
+          "../../lib/package/bundleLinter.js",
+        ),
+        destBl = path.resolve(
+          tmpdir.name,
+          "node_modules/apigeelint/lib/package/bundleLinter.js",
+        );
+      fs.cpSync(srcBl, destBl, { force: true });
       try {
         const proc = child_process.spawn(
           "node",
-          [path.resolve(node_modules, "apigeelint/cli.js"), ...cliOpts],
+          [
+            path.resolve(tmpdir.name, "node_modules/apigeelint/cli.js"),
+            ...cliOpts,
+          ],
           { ...spawnOpts, timeout: 20000 },
         );
         let stdoutBlobs = [],
@@ -86,10 +104,18 @@ describe("cli options handling", function () {
           if (code != 0) {
             // Could examine this - with DEBUG=apigeelint:rc, you will see
             // messages indicating parsing of the .apigeelintrc file.
-            debug(`stderr: ${aggregatedStderr}`);
+            debug(`agg stderr: ${aggregatedStderr}`);
           }
+          debug(`agg stdout: ${aggregatedStdout}`);
           // we should be able to parse stdout as json
-          const items = JSON.parse(aggregatedStdout);
+          let items = [];
+          try {
+            items = JSON.parse(aggregatedStdout);
+          } catch (eparse) {
+            console.log(`while parsing: ${aggregatedStdout}`);
+            console.log(eparse.stack);
+            assert.fail();
+          }
           checkCb(code, items);
         });
       } catch (ex1) {
@@ -100,7 +126,7 @@ describe("cli options handling", function () {
   };
 
   it("should use the default configuration with --norc", function (done) {
-    this.timeout(58000);
+    this.timeout(158000);
     if (debug.enabled) {
       const r = child_process.spawnSync("which", ["node"], spawnOpts);
       debug(`node: ` + JSON.stringify(r));
@@ -108,32 +134,38 @@ describe("cli options handling", function () {
 
     // run apigeelint after npm install with --norc, it should use
     // default profile and default formatter.
-    runOne(["-s", "just-warnings/apiproxy", "--norc"], (code, items) => {
-      // console.log(JSON.stringify(items, null, 2));
-      // expect status code 0 = apigeelint found only a few warnings
-      assert.equal(code, 0, "return status code");
-      const itemsWithMessages = items.filter((item) => item.messages.length);
-      assert.equal(
-        itemsWithMessages.length,
-        3,
-        "number of items with messages",
-      );
-      const target1Items = itemsWithMessages.filter((item) =>
-        item.filePath.endsWith("/apiproxy/targets/target-1.xml"),
-      );
-      assert.equal(target1Items.length, 1);
-      assert.ok(target1Items[0].messages.length);
-      // with --norc, the default is profile=apigee
-      assert.ok(
-        target1Items[0].messages.find((m) =>
-          m.message.endsWith("on profile=apigee"),
-        ),
-      );
-      // assert that we also found TD002, TD007 errors
-      assert.ok(target1Items[0].messages.find((m) => m.ruleId == "TD007"));
-      assert.ok(target1Items[0].messages.find((m) => m.ruleId == "TD002"));
-      done();
-    });
+    runOne(
+      ["-s", path.resolve(issue608Dir, "just-warnings/apiproxy"), "--norc"],
+      (code, items) => {
+        // console.log("items: " + JSON.stringify(items, null, 2));
+        // status code 0 = apigeelint found zero errors, and possibly a few warnings
+        // status code 1 = apigeelint found at least one error, or more than a few warnings
+        assert.equal(code, 1, "return status code");
+        const itemsWithMessages = items.filter((item) => item.messages.length);
+        assert.equal(
+          itemsWithMessages.length,
+          3,
+          "number of items with messages",
+        );
+        const target1Items = itemsWithMessages.filter((item) =>
+          item.filePath.endsWith(
+            path.normalize("/apiproxy/targets/target-1.xml"),
+          ),
+        );
+        assert.equal(target1Items.length, 1);
+        assert.ok(target1Items[0].messages.length);
+        // with --norc, the default is profile=apigee
+        assert.ok(
+          target1Items[0].messages.find((m) =>
+            m.message.endsWith("on profile=apigee"),
+          ),
+        );
+        // assert that we also found TD002, TD007 errors
+        assert.ok(target1Items[0].messages.find((m) => m.ruleId == "TD007"));
+        assert.ok(target1Items[0].messages.find((m) => m.ruleId == "TD002"));
+        done();
+      },
+    );
   });
 
   it("should ingest the right configuration with apigeelintrc", function (done) {
@@ -144,27 +176,31 @@ describe("cli options handling", function () {
     }
 
     // Run apigeelint after npm install; it should find and use the apigeelintrc file.
-    runOne(["-s", "just-warnings/apiproxy"], (code, items) => {
-      // expect status code 0 = apigeelint found only a few warnings
-      assert.equal(code, 0, "return status code");
-      // console.log(JSON.stringify(items, null, 2));
-      const itemsWithMessages = items.filter((item) => item.messages.length);
-      assert.equal(
-        itemsWithMessages.length,
-        2,
-        "number of items with messages",
-      );
-      const target1Items = itemsWithMessages.filter((item) =>
-        item.filePath.endsWith("/apiproxy/targets/target-1.xml"),
-      );
-      assert.equal(target1Items.length, 0);
-      // assert no TD002, TD007 errors. Such warnings would appear on the
-      // target, and we've already determined that the target-1 has no warnings,
-      // so this check is redundant.
-      assert.ok(!items[0].messages.find((m) => m.ruleId == "TD007"));
-      assert.ok(!items[0].messages.find((m) => m.ruleId == "TD002"));
-      done();
-    });
+    runOne(
+      ["-s", path.resolve(issue608Dir, "just-warnings/apiproxy")],
+      (code, items) => {
+        // console.log("items: " + JSON.stringify(items, null, 2));
+        assert.equal(code, 1, "return status code");
+        const itemsWithMessages = items.filter((item) => item.messages.length);
+        assert.equal(
+          itemsWithMessages.length,
+          2,
+          "number of items with messages",
+        );
+        const target1Items = itemsWithMessages.filter((item) =>
+          item.filePath.endsWith(
+            path.normalize("/apiproxy/targets/target-1.xml"),
+          ),
+        );
+        assert.equal(target1Items.length, 0);
+        // assert no TD002, TD007 errors. Such warnings would appear on the
+        // target, and we've already determined that the target-1 has no warnings,
+        // so this check is redundant.
+        assert.ok(!items[0].messages.find((m) => m.ruleId == "TD007"));
+        assert.ok(!items[0].messages.find((m) => m.ruleId == "TD002"));
+        done();
+      },
+    );
   });
 
   it("should exit with a non-zero exit code when it finds errors", function (done) {
@@ -173,10 +209,13 @@ describe("cli options handling", function () {
       const r = child_process.spawnSync("which", ["node"], spawnOpts);
       debug(`node: ` + JSON.stringify(r));
     }
-    runOne(["-s", "some-errors/apiproxy"], (code, items) => {
-      // expect status code 1 = apigeelint found at least one error
-      assert.equal(code, 1, "return status code");
-      done();
-    });
+    runOne(
+      ["-s", path.resolve(issue608Dir, "some-errors/apiproxy")],
+      (code, items) => {
+        // expect status code 1 = apigeelint found at least one error
+        assert.equal(code, 1, "return status code");
+        done();
+      },
+    );
   });
 });
