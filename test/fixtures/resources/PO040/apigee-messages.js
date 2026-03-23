a6f46e0b0c816cd4c0a784141042466b08afcfaa
DinoChiesa <dpchiesa@hotmail.com>
2026-03-22 18:09:22 -0700

    fix: PO040 non-compiling jsonpath results in warning

--- test/fixtures/resources/PO040/apigee-messages.js
+++ test/fixtures/resources/PO040/apigee-messages.js
@@ -1,13 +1,37 @@
-// These are the error messages only for the policies that will get flagged, in the Apigee profile.
+// These are the error or warning messages only for the policies that will get
+// flagged, in the Apigee profile.
+//
+// The value of each key can be:
+//  - a single item, either a string or an object
+//  - an array of strings or objects.
+//
+// If a bare string, no severity is indicated, it defaults to 2.
+//
 
 module.exports = {
   "EV-JSON-4.xml": [
-    "JSONPath ($.store.book[0].[author) is invalid (Error: Parse error on line 1",
-    "JSONPath ($.store.book[0].[]author) is invalid (Error: Parse error on line 1",
+    {
+      severity: 1,
+      message:
+        "JSONPath ($.store.book[0].[author) does not compile, may be invalid (Error: Parse error on line 1",
+    },
+    {
+      severity: 1,
+      message:
+        "JSONPath ($.store.book[0].[]author) does not compile, may be invalid (Error: Parse error on line 1",
+    },
   ],
   "EV-JSON-5.xml": "JSONPath is empty",
-  "EV-JSON-6.xml":
-    "JSONPath ($.quota.[*].appname..) is invalid (Error: Parse error on line 1",
+  "EV-JSON-6.xml": {
+    severity: 1,
+    message:
+      "JSONPath ($.quota.[*].appname..) does not compile, may be invalid (Error: Parse error on line 1",
+  },
+  "EV-JSON-7.xml": {
+    severity: 1,
+    message:
+      "JSONPath ($.selected-index) does not compile, may be invalid (Error: ",
+  },
   "EV-JSONPayload-curlies-unbalanced.xml": [
     "JSONPath '$.{foo' uses unbalanced or invalid curly braces: unclosed curly",
   ],
